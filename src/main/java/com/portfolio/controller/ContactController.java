86dceeb6ceb02b82149242f10fdd280b4826f978
Anjali-2412 <naina.jain3333@gmail.com>
2025-05-16 19:07:27 +0530

    pushed entire project with updated Html,CSS, and JS

--- src/main/java/com/portfolio/controller/ContactController.java
+++ src/main/java/com/portfolio/controller/ContactController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000"})
+@CrossOrigin(origins = "*")
 public class ContactController {
 
     @PostMapping
